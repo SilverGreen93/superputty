b16c10d57266c46e8e4cfcd1632369fca50f33d2
SilverGreen93 <mihai.vasiliu.93@gmail.com>
2026-02-21 17:02:51 +0200

    * 'Line 230: Fixes CS1503 by explicitly casting data.ItemData to System.Data.DataTable, which is required by the DataView constructor.' in file 'SuperPutty\Gui\QuickSelector.cs'

--- SuperPutty/Gui/QuickSelector.cs
+++ SuperPutty/Gui/QuickSelector.cs
@@ -227,7 +227,7 @@ namespace SuperPutty.Gui
         {
             // bind data
             this.Options = options;
-            this.DataView = new DataView(data.ItemData) {Sort = options.Sort};
+            this.DataView = new DataView((System.Data.DataTable)data.ItemData) {Sort = options.Sort};
             this.dataGridViewData.DataSource = this.DataView;
 
             // configure grid
